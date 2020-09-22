92bbcf6b9aa6730bde4f67af4bc4b0767dd71500
Blake A. Long <blake.a.long@gmail.com>
2020-09-22 15:38:05 -0600

    merge sort

---
+++ MergeSort/src/com/blakelong/Main.java
@@ -0,0 +1,43 @@
+package com.blakelong;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int[] intArray = {43, 3, -34, 5, 12, 22, -12};
+
+	    mergeSort(intArray, 0, intArray.length);
+        System.out.println(Arrays.toString(intArray));
+    }
+
+    public static void mergeSort(int[] array, int start, int end) {
+        if (end - start < 2) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        mergeSort(array, start, mid);
+        mergeSort(array, mid, end);
+        merge(array, start, mid, end);
+    }
+
+    public static void merge(int[] array, int start, int mid, int end) {
+        if (array[mid - 1] <= array[mid]) {
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int[] temp = new int[end - start];
+        while (i < mid && j < end) {
+            temp[tempIndex++] = array[i] <= array[j] ? array[i++] : array[j++];
+        }
+
+        System.arraycopy(array, i, array, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, array, start, tempIndex);
+
+    }
+}
